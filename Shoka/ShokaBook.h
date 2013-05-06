57ede976a0f450c3ed9b46ce3950b5fc9e697bda
Xhacker Liu <liu.dongyuan@gmail.com>
2013-05-06 22:54:16 +0800

    Declared ShokaBook public API.

--- Shoka/ShokaBook.h
+++ Shoka/ShokaBook.h
@@ -11,8 +11,14 @@
 @interface ShokaBook : NSObject
 
 @property (strong, nonatomic) NSString *title;
-@property (strong, nonatomic) NSString *isbn;
+@property (strong, nonatomic) NSString *ISBN;
 @property (strong, nonatomic) NSString *publisher;
+@property (nonatomic) NSInteger publishYear;
+@property (strong, nonatomic) NSString *author;
+@property (strong, nonatomic) NSString *translator;
+@property (strong, nonatomic) NSString *summary;
+@property (strong, nonatomic) NSString *subject;
+@property (nonatomic) NSInteger pages;
 @property (strong, nonatomic) NSMutableDictionary *extraInfo;
 
 @end
